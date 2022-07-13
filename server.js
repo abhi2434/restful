d8fc38438d102942bd6be090c737ac3acebfe0f9
Abhishek Sur <contact@abhisheksur.com>
2022-07-14 00:13:00 +0530

    Update : fixed the port number

--- server.js
+++ server.js
@@ -54,7 +54,7 @@ app.post('/addUser', function (req, res) {
        res.end( JSON.stringify(data));
     });
  })
-var server = app.listen(8081, function () {
+var server = app.listen(80, function () {
    var host = server.address().address
    var port = server.address().port
    console.log("Example app listening at http://%s:%s", host, port)
